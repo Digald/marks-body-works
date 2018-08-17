19be8e14e31a1b164aaf3d3c08d74afebfc71b22
Mark Alaniz <markalaniz000@gmail.com>
2018-08-17 03:00:09 -0500

    Added most of the href and links to landing page. Need to work on reponsiveness and styles a bit more before moving on.

--- imports/client/Components/HeaderNav.js
+++ imports/client/Components/HeaderNav.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-const HeaderNav = () => (
-    <div className="HeaderNav">
-        <p className="HeaderNav__item">Github</p>
-        <p className="HeaderNav__item">LinkedIn</p>
-        <p className="HeaderNav__item">Login</p>
-        <p className="HeaderNav__item">Sign Up</p>
-    </div>
-)
+class HeaderNav extends Component {
+  render() {
+    return (
+      <div className="HeaderNav">
+        <a target="_blank" href="https://github.com/Digald">
+          <p className="HeaderNav__item">Github</p>
+        </a>
+        <a target="_blank" href="https://www.linkedin.com/in/markalaniz/">
+          <p className="HeaderNav__item">LinkedIn</p>
+        </a>
+        <Link to="/">
+          <p className="HeaderNav__item">Login</p>
+        </Link>
+        <Link to="/">
+          <p className="HeaderNav__item">Sign Up</p>
+        </Link>
+      </div>
+    );
+  }
+}
 
 export default HeaderNav;
--- imports/client/Components/MainNav.js
+++ imports/client/Components/MainNav.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const MainNav = () => (
   <div className="MainNav">
-    <h1 className="MainNav__logo">LOGO</h1>
+    <Link to="/">
+      <h1 className="MainNav__logo">Mark's Body Works</h1>
+    </Link>
     <div className="MainNav__main-items">
-      <p className="MainNav__main-items__item">Power BB</p>
-      <p className="MainNav__main-items__item">5/3/1 Simplest Template</p>
+      <Link to="/programs/powerbb">
+        <p className="MainNav__main-items__item">Power BB</p>
+      </Link>
+      <Link to="programs/531">
+        <p className="MainNav__main-items__item MainNav__main-items__item-last">5/3/1 Simplest Template</p>
+      </Link>
     </div>
   </div>
 );
