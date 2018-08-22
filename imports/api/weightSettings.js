cbec69ae5dad621b8f014d7888e43836f7b801b7
Mark Alaniz <markalaniz000@gmail.com>
2018-08-21 19:10:00 -0500

    Correct schema, added meteor method to insert weights for the first time, updated form to submit weights and insert into database. Still trying to get a reponse back after inserting.

--- imports/api/weightSettings.js
+++ imports/api/weightSettings.js
@@ -5,6 +5,17 @@ import SimpleSchema from "simpl-schema";
 export const WeightSettings = new Mongo.Collection("weightSettings");
 
 const PowerbbSchema = new SimpleSchema({
+  workoutWeek: {
+    type: String,
+    required: false
+  }
+});
+
+const WeightSettingsSchema = new SimpleSchema({
+  user: {
+    type: String,
+    required: false
+  },
   overheadMax: {
     type: SimpleSchema.Integer,
     defaultValue: 0
@@ -21,19 +32,14 @@ const PowerbbSchema = new SimpleSchema({
     type: SimpleSchema.Integer,
     defaultValue: 0
   },
-  workoutWeek: String,
   lastUpdated: {
     type: Date,
     defaultValue: new Date()
-  }
-});
-
-const WeightSettingsSchema = new SimpleSchema({
-  user: {
-      type: String,
-      required: false
   },
-  powerbb: PowerbbSchema
+  powerbb: {
+    type: PowerbbSchema,
+    required: false
+  }
 });
 
 WeightSettings.attachSchema(WeightSettingsSchema);
@@ -41,5 +47,17 @@ WeightSettings.attachSchema(WeightSettingsSchema);
 if (Meteor.isServer) {
   Meteor.publish("allWeights", function() {
     return WeightSettings.find({});
+  });
+
+  Meteor.methods({
+    async insertRepMaxes(overheadMax, benchMax, squatMax, deadliftMax) {
+      await WeightSettings.insert({
+        overheadMax,
+        benchMax,
+        squatMax,
+        deadliftMax,
+        lastUpdated: new Date()
+      });
+    }
   });
 }
--- imports/client/Components/RepMaxForms.js
+++ imports/client/Components/RepMaxForms.js
@@ -1,32 +1,63 @@
 import React, { Component } from "react";
 import SectionTitle from "./SectionTitle";
-
-import { withTracker } from 'meteor/react-meteor-data';
+import ReactDOM from "react-dom";
+// Meteor Imports
+import { withTracker } from "meteor/react-meteor-data";
 import { WeightSettings } from "../../api/weightSettings";
 
 class RepMaxForms extends Component {
+  handleSubmit(e) {
+    e.preventDefault();
+    const squatMax = parseInt(
+      ReactDOM.findDOMNode(this.refs.squatmax).value.trim()
+    );
+    const benchMax = parseInt(
+      ReactDOM.findDOMNode(this.refs.benchmax).value.trim()
+    );
+    const deadliftMax = parseInt(
+      ReactDOM.findDOMNode(this.refs.deadmax).value.trim()
+    );
+    const overheadMax = parseInt(
+      ReactDOM.findDOMNode(this.refs.ohpmax).value.trim()
+    );
+    Meteor.call(
+      "insertRepMaxes",
+      overheadMax,
+      benchMax,
+      squatMax,
+      deadliftMax,
+      (err, res) => {
+        if (err) console.log(err);
+        console.log(res);
+      }
+    );
+  }
+
   render() {
     return (
       <div className="RepMaxForms">
         <SectionTitle title={"1 Rep Max"} />
-        <form>
+        <form id="repmaxform" onSubmit={e => this.handleSubmit(e)}>
           <label>
-            <input name="squat" type="text" />
+            <input name="squat" type="text" ref="squatmax" />
             Squat
           </label>
           <label>
-            <input name="bench" type="text" />
+            <input name="bench" type="text" ref="benchmax" />
             Bench
           </label>
           <label>
-            <input name="dead" type="text" />
+            <input name="dead" type="text" ref="deadmax" />
             Deadlift
           </label>
           <label>
-            <input name="ohp" type="text" />
+            <input name="ohp" type="text" ref="ohpmax" />
             Overhead Press
           </label>
         </form>
+        <button type="submit" form="repmaxform">
+          Submit
+        </button>
       </div>
     );
   }
