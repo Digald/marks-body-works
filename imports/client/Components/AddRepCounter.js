11d36f204e368c49ba0ae1e602d60a58916a703d
Mark Alaniz <markalaniz000@gmail.com>
2018-08-27 20:18:46 -0500

    Figuring out how to dynamically add elements one at a time

--- imports/client/Components/AddRepCounter.js
+++ imports/client/Components/AddRepCounter.js
@@ -1,11 +1,40 @@
 import React, { Component } from "react";
 
 class AddRepCounter extends Component {
+  state = {
+    counterArr: [],
+    key: 0
+  };
+
+  addRepCounter(e, counter) {
+    e.preventDefault();
+    if (counter === "+") {
+      const symbol = "√";
+      this.setState({ counterArr: [this.state.counterArr, ...symbol], key: this.state.key + 1 });
+    } else if (counter === "-") {
+      const symbol = "x";
+      this.setState({ counterArr: [this.state.counterArr, ...symbol], key: this.state.key + 1 });
+    }
+  }
+
   render() {
     return (
       <span className="AddRepCounter">
-        <button className="AddRepCounter__success">+</button>
-        <button className="AddRepCounter__fail">x</button>
+        {this.state.counterArr.map(element => {
+          return <p key={this.state.key}>{element}</p>;
+        })}
+        <button
+          onClick={e => this.addRepCounter(e, "+")}
+          className="AddRepCounter__success"
+        >
+          +
+        </button>
+        <button
+          onClick={e => this.addRepCounter(e, "-")}
+          className="AddRepCounter__fail"
+        >
+          -
+        </button>
       </span>
     );
   }
